e45ef412543a152d3447baf4b9aa2e5fdf259661
json-alzate <developer.alzate@gmail.com>
2022-04-05 21:47:41 -0500

    🌐 Get and change automatic lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@
 import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { TranslocoService } from '@ngneat/transloco';
+import { ModalController } from '@ionic/angular';
+import { Device } from '@capacitor/device';
+
 
 // rxjs
 import { Subject, Observable } from 'rxjs';
@@ -21,6 +24,7 @@ import { Profile } from '@models/profile.model';
 // services
 
 // components
+import { LoginComponent } from '@shared/components/login/login.component';
 
 @Component({
   selector: 'app-root',
@@ -32,11 +36,33 @@ export class AppComponent {
   profile$: Observable<Profile>;
 
   constructor(
+    private modalController: ModalController,
     private translocoService: TranslocoService,
     private store: Store<AuthState>
   ) {
+
     this.profile$ = this.store.pipe(
       select(getProfile)
     );
+    this.getLang();
+  }
+
+  async getLang() {
+    const lang = await Device.getLanguageCode();
+    
+    if( lang.value.slice(0,2) === 'es' ){
+      this.translocoService.setActiveLang('es');
+    }
+
+  }
+
+  async presentModalLogin() {
+    const modal = await this.modalController.create({
+      component: LoginComponent,
+      componentProps: { value: 123 }
+    });
+
+    await modal.present();
+
   }
 }
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,9 +1,12 @@
 import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { LoginComponent } from './login/login.component';
 
 export const COMPONENTS = [
-    HeaderBarComponent
+    HeaderBarComponent,
+    LoginComponent
 ];
 
 export const ENTRY_COMPONENTS: any[] = [
-    HeaderBarComponent
+    HeaderBarComponent,
+    LoginComponent
 ];
