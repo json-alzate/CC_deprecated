fd976639d9c2531b72d287b3b00f61d8458540b0
json-alzate <developer.alzate@gmail.com>
2022-07-05 19:45:37 -0500

    💄on ui clock and options

--- src/app/pages/coordinates/coordinates.page.ts
+++ src/app/pages/coordinates/coordinates.page.ts
@@ -32,6 +32,10 @@ import {
 })
 export class CoordinatesPage implements OnInit {
 
+  letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  number = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+  currentPuzzle = '';
 
   board;
 
