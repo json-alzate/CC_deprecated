96bb17ec9f116edc13c9f1b2a84aad2b6bedef55
Json Alzate <developer.alzate@gmail.com>
2023-06-22 20:56:00 -0500

    🍱sounds

--- src/app/pages/squares/squares.page.ts
+++ src/app/pages/squares/squares.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ElementRef, ViewChildren, QueryList, AfterViewInit } from '@angular/core';
 import { IonCard, Platform, Gesture, GestureController } from '@ionic/angular';
+import { timeout } from 'rxjs';
+
+interface SquareColorMap {
+  name: string;
+  color: string;
+}
 
 @Component({
   selector: 'app-squares',
@@ -9,22 +15,37 @@ import { IonCard, Platform, Gesture, GestureController } from '@ionic/angular';
 export class SquaresPage implements OnInit, AfterViewInit {
 
   // Tus casillas
-  public squares = ['a1', 'b2', 'c3', 'd4', 'e5', 'f6', 'g7', 'h8'];
+  public squares: SquareColorMap[] = [];
+  squaresColorsMap: SquareColorMap[] = this.getSquaresColorsMap();
+  gameStatus: 'waiting' | 'playing' | 'win' | 'lose' = 'waiting';
 
   @ViewChildren(IonCard, { read: ElementRef }) cards: QueryList<ElementRef>;
 
   constructor(
     private platform: Platform,
     private gestureCtrl: GestureController
-  ) { }
+  ) {
+    this.generateSquares();
+  }
 
   ngOnInit() {
-    // tu código de inicialización aquí
   }
 
   ngAfterViewInit() {
     const cardArray = this.cards.toArray();
     this.useSwiperGesture(cardArray);
+  }
+
+  generateSquares() {
+    console.log(this.squaresColorsMap);
+
+    // genera 1000 casillar aleatorias con su correspondiente color
+    for (let i = 0; i < 1000; i++) {
+      const randomIndex = Math.floor(Math.random() * this.squaresColorsMap.length);
+      this.squares.push(this.squaresColorsMap[randomIndex]);
+    }
+    console.log(this.squares);
+
   }
 
   useSwiperGesture(cardArray: ElementRef[]) {
@@ -40,21 +61,71 @@ export class SquaresPage implements OnInit, AfterViewInit {
           card.nativeElement.style.transform = `translateX(${ev.deltaX}px) rotate(${ev.deltaX / 10}deg)`;
         },
         onEnd: ev => {
+          let movement: 'right' | 'left';
+
           card.nativeElement.style.transition = '.5s ease-out';
 
-          if (ev.deltaX > 150) {
+          if (ev.deltaX > 250) {
             card.nativeElement.style.transform = `translateX(${this.platform.width() * 2}px) rotate(${ev.deltaX / 2}deg)`;
             // Aquí puedes agregar la lógica para cuando el usuario adivina que la casilla es blanca
-          } else if (ev.deltaX < -150) {
+            movement = 'right';
+
+          } else if (ev.deltaX < -250) {
             card.nativeElement.style.transform = `translateX(-${this.platform.width() * 2}px) rotate(${ev.deltaX / 2}deg)`;
             // Aquí puedes agregar la lógica para cuando el usuario adivina que la casilla es negra
+            movement = 'left';
           } else {
             card.nativeElement.style.transform = '';
           }
+          if (movement) {
+
+            if (this.squares[i].color === 'white' && movement === 'right') {
+              this.gameStatus = 'win';
+            } else if (this.squares[i].color === 'black' && movement === 'left') {
+              this.gameStatus = 'win';
+            } else {
+              this.gameStatus = 'lose';
+            }
+
+            console.log(this.gameStatus);
+
+
+            // setTimeout(() => {
+            //   this.gameStatus = 'playing';
+            // }, 500);
+
+          }
+
+
+          // this.checkGuess(movement, this.squares[i].name);
         }
+
       });
 
       gesture.enable(true);
     }
   }
+
+
+  getSquaresColorsMap(): SquareColorMap[] {
+    // Aquí puedes agregar la lógica para saber si la casilla es blanca o negra
+    const squaresColorsMap: SquareColorMap[] = [];
+
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    const colors = ['white', 'black'];
+
+    for (let row = 1; row <= 8; row++) {
+      for (let col = 0; col < 8; col++) {
+        const square = letters[col] + row;
+        const color = colors[(row + col) % 2];
+        const toAdd = { name: square, color };
+        squaresColorsMap.push(toAdd);
+      }
+    }
+
+    return squaresColorsMap;
+
+  }
+
+
 }
