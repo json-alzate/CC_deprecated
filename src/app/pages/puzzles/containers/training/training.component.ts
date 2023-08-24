d6dda5e66321fad57788953f6042552a8c386ed3
Json Alzate <developer.alzate@gmail.com>
2023-08-24 07:58:14 -0500

    ⚡️Build board method

--- src/app/pages/puzzles/containers/training/training.component.ts
+++ src/app/pages/puzzles/containers/training/training.component.ts
@@ -19,7 +19,6 @@ import { ARROW_TYPE, Arrows } from 'cm-chessboard/src/extensions/arrows/arrows';
 import { PromotionDialog } from 'cm-chessboard/src/extensions/promotion-dialog/PromotionDialog';
 import Chess from 'chess.js';
 import { createUid } from '@utils/create-uid';
-import { randomNumber } from '@utils/random-number';
 import { calculateElo } from '@utils/calculate-elo';
 
 // rxjs
@@ -103,7 +102,6 @@ export class TrainingComponent implements OnInit {
   private unsubscribe$ = new Subject<void>();
   private unsubscribeIntervalSeconds$ = new Subject<void>();
 
-  // FIXME: no se realiza el enroque bien, solo se mueve el rey las dos casillas pero la torre no se mueve
 
   constructor(
     private puzzlesService: PuzzlesService,
@@ -166,7 +164,7 @@ export class TrainingComponent implements OnInit {
     console.log(this.appService.pieces);
     // espera un segundo para que se destruya el tablero
     setTimeout(() => {
-      this.loadBoard();
+      this.buildBoard(this.chessInstance.fen());
     }, 1000);
 
   }
@@ -280,175 +278,7 @@ export class TrainingComponent implements OnInit {
     // Se valida si es la primera vez que se carga el tablero
     if (!this.board) {
 
-      const uniqueTimestamp = new Date().getTime();
-      const piecesPath = `${this.appService.pieces}?t=${uniqueTimestamp}`;;
-      console.log(piecesPath);
-
-
-      this.board = new Chessboard(document.getElementById('boardPuzzle'), {
-        responsive: true,
-        position: this.puzzleToResolve.fen,
-        assetsUrl: '/assets/cm-chessboard/',
-        assetsCache: true,
-        style: {
-          // cssClass: 'chessboard-js',
-          borderType: BORDER_TYPE.thin,
-          pieces: {
-            file: piecesPath
-          }
-        },
-        extensions: [
-          { class: Markers },
-          { class: Arrows },
-          { class: PromotionDialog }
-        ]
-      });
-
-      this.board.enableMoveInput((event) => {
-
-        // handle user input here
-        switch (event.type) {
-
-          case 'moveInputStarted':
-            // mostrar indicadores para donde se puede mover la pieza
-            this.board.removeMarkers();
-            this.board.removeArrows();
-
-            if (this.chessInstance.turn() === this.puzzleColor && this.chessInstance.moves({ square: event.square }).length > 0) {
-              // adiciona el marcador para la casilla seleccionada
-              const markerSquareSelected = { class: 'marker-square-green', slice: 'markerSquare' };
-              this.board.addMarker(markerSquareSelected, event.square);
-              const possibleMoves = this.chessInstance.moves({ square: event.square, verbose: true });
-              for (const move of possibleMoves) {
-                const markerDotMove = { class: 'marker-dot-green', slice: 'markerDot' };
-                this.board.addMarker(markerDotMove, move.to);
-              }
-            }
-            return true;
-          case 'validateMoveInput':
-
-
-            if ((event.squareTo.charAt(1) === '8' || event.squareTo.charAt(1) === '1') && event.piece.charAt(1) === 'p') {
-
-              const colorToShow = event.piece.charAt(0) === 'w' ? COLOR.white : COLOR.black;
-
-              this.board.showPromotionDialog(event.squareTo, colorToShow, (result) => {
-                if (result && result.piece) {
-                  this.board.setPiece(result.square, result.piece, true);
-                  // remover la piece de la casilla de origen
-                  this.board.setPiece(event.squareFrom, undefined, true);
-                  const objectMovePromotion = { from: event.squareFrom, to: event.squareTo, promotion: result.piece.charAt(1) };
-                  const theMovePromotion = this.chessInstance.move(objectMovePromotion);
-                  if (theMovePromotion) {
-                    this.validateMove();
-                  }
-                } else {
-                  console.log('Promotion canceled');
-                }
-              });
-            }
-
-            const objectMove = { from: event.squareFrom, to: event.squareTo };
-            const theMove = this.chessInstance.move(objectMove);
-
-            if (theMove) {
-              this.validateMove();
-            }
-            // return true, if input is accepted/valid, `false` takes the move back
-            return theMove;
-          case 'moveInputCanceled':
-            // hide the indicators
-            return true;
-          case 'moveInputFinished':
-            this.board.removeMarkers();
-            this.board.removeArrows();
-            break;
-          default:
-            return true;
-        }
-      });
-
-
-      let startSquare;
-      let endSquare;
-      this.board.enableSquareSelect((event) => {
-
-
-        const ctrKeyPressed = event.mouseEvent.ctrlKey;
-        const shiftKeyPressed = event.mouseEvent.shiftKey;
-        const altKeyPressed = event.mouseEvent.altKey;
-
-        if (event.mouseEvent.type === 'mousedown' && event.mouseEvent.which === 3) { // click derecho
-          startSquare = event.square;
-        }
-
-
-        if (event.mouseEvent.type === 'mouseup' && event.mouseEvent.which === 3) { // liberar click derecho
-          endSquare = event.square;
-
-          if (startSquare === endSquare) {
-            return;
-          }
-
-          // Ahora, dibujamos la flecha usando el inicio y el final de las coordenadas
-          let arrowType = {
-            class: 'arrow-green',
-            headSize: 7,
-            slice: 'arrowDefault'
-          };
-
-          if (shiftKeyPressed) {
-            arrowType = { ...arrowType, class: 'arrow-blue' };
-          } else if (altKeyPressed) {
-            arrowType = { ...arrowType, class: 'arrow-yellow' };
-          } else if (ctrKeyPressed) {
-            arrowType = { ...arrowType, class: 'arrow-red' };
-          }
-
-
-          this.board.addArrow(arrowType, startSquare, endSquare);
-        }
-
-
-        if (event.type === SQUARE_SELECT_TYPE.primary && event.mouseEvent.type === 'mousedown') {
-
-          if (!this.chessInstance.get(event.square)) {
-            this.board.removeArrows();
-            this.board.removeMarkers();
-          }
-
-        }
-
-        if (event.type === SQUARE_SELECT_TYPE.secondary && event.mouseEvent.type === 'mousedown') {
-
-          let classCircle = 'marker-circle-green';
-
-          if (ctrKeyPressed) {
-            classCircle = 'marker-circle-red';
-          } else if (shiftKeyPressed) {
-            classCircle = 'marker-circle-blue';
-          } else if (altKeyPressed) {
-            classCircle = 'marker-circle-yellow';
-          }
-
-          let myOwnMarker = { class: classCircle, slice: 'markerCircle' };
-
-          if (ctrKeyPressed && shiftKeyPressed && altKeyPressed) {
-            myOwnMarker = MARKER_TYPE.frame;
-          }
-
-
-          const markersOnSquare = this.board.getMarkers(undefined, event.square);
-          if (markersOnSquare.length > 0) {
-            this.board.removeMarkers(undefined, event.square);
-          } else {
-
-            this.board.addMarker(myOwnMarker, event.square);
-          }
-
-
-        }
-      });
+      this.buildBoard(this.puzzleToResolve.fen);
 
     } else {
       // Ya el tablero fue cargado la primera vez
@@ -457,7 +287,6 @@ export class TrainingComponent implements OnInit {
 
 
 
-    this.turnRoundBoard(this.puzzleColor);
     this.puzzleMoveResponse();
     this.initTimer();
   }
@@ -467,7 +296,180 @@ export class TrainingComponent implements OnInit {
   /**
    * Build board ui
    */
-  buildBoard() {
+  buildBoard(fen: string) {
+
+    const uniqueTimestamp = new Date().getTime();
+    const piecesPath = `${this.appService.pieces}?t=${uniqueTimestamp}`;;
+    console.log(piecesPath);
+
+
+    this.board = new Chessboard(document.getElementById('boardPuzzle'), {
+      responsive: true,
+      position: fen,
+      assetsUrl: '/assets/cm-chessboard/',
+      assetsCache: true,
+      style: {
+        // cssClass: 'chessboard-js',
+        borderType: BORDER_TYPE.thin,
+        pieces: {
+          file: piecesPath
+        }
+      },
+      extensions: [
+        { class: Markers },
+        { class: Arrows },
+        { class: PromotionDialog }
+      ]
+    });
+
+    this.board.enableMoveInput((event) => {
+
+      // handle user input here
+      switch (event.type) {
+
+        case 'moveInputStarted':
+          // mostrar indicadores para donde se puede mover la pieza
+          this.board.removeMarkers();
+          this.board.removeArrows();
+
+          if (this.chessInstance.turn() === this.puzzleColor && this.chessInstance.moves({ square: event.square }).length > 0) {
+            // adiciona el marcador para la casilla seleccionada
+            const markerSquareSelected = { class: 'marker-square-green', slice: 'markerSquare' };
+            this.board.addMarker(markerSquareSelected, event.square);
+            const possibleMoves = this.chessInstance.moves({ square: event.square, verbose: true });
+            for (const move of possibleMoves) {
+              const markerDotMove = { class: 'marker-dot-green', slice: 'markerDot' };
+              this.board.addMarker(markerDotMove, move.to);
+            }
+          }
+          return true;
+        case 'validateMoveInput':
+
+
+          if ((event.squareTo.charAt(1) === '8' || event.squareTo.charAt(1) === '1') && event.piece.charAt(1) === 'p') {
+
+            const colorToShow = event.piece.charAt(0) === 'w' ? COLOR.white : COLOR.black;
+
+            this.board.showPromotionDialog(event.squareTo, colorToShow, (result) => {
+              if (result && result.piece) {
+                this.board.setPiece(result.square, result.piece, true);
+                // remover la piece de la casilla de origen
+                this.board.setPiece(event.squareFrom, undefined, true);
+                const objectMovePromotion = { from: event.squareFrom, to: event.squareTo, promotion: result.piece.charAt(1) };
+                const theMovePromotion = this.chessInstance.move(objectMovePromotion);
+                if (theMovePromotion) {
+                  this.validateMove();
+                }
+              } else {
+                console.log('Promotion canceled');
+              }
+            });
+          }
+
+          const objectMove = { from: event.squareFrom, to: event.squareTo };
+          const theMove = this.chessInstance.move(objectMove);
+
+          if (theMove) {
+            this.validateMove();
+          }
+          // return true, if input is accepted/valid, `false` takes the move back
+          return theMove;
+        case 'moveInputCanceled':
+          // hide the indicators
+          return true;
+        case 'moveInputFinished':
+          this.board.removeMarkers();
+          this.board.removeArrows();
+          break;
+        default:
+          return true;
+      }
+    });
+
+
+    let startSquare;
+    let endSquare;
+    this.board.enableSquareSelect((event) => {
+
+
+      const ctrKeyPressed = event.mouseEvent.ctrlKey;
+      const shiftKeyPressed = event.mouseEvent.shiftKey;
+      const altKeyPressed = event.mouseEvent.altKey;
+
+      if (event.mouseEvent.type === 'mousedown' && event.mouseEvent.which === 3) { // click derecho
+        startSquare = event.square;
+      }
+
+
+      if (event.mouseEvent.type === 'mouseup' && event.mouseEvent.which === 3) { // liberar click derecho
+        endSquare = event.square;
+
+        if (startSquare === endSquare) {
+          return;
+        }
+
+        // Ahora, dibujamos la flecha usando el inicio y el final de las coordenadas
+        let arrowType = {
+          class: 'arrow-green',
+          headSize: 7,
+          slice: 'arrowDefault'
+        };
+
+        if (shiftKeyPressed) {
+          arrowType = { ...arrowType, class: 'arrow-blue' };
+        } else if (altKeyPressed) {
+          arrowType = { ...arrowType, class: 'arrow-yellow' };
+        } else if (ctrKeyPressed) {
+          arrowType = { ...arrowType, class: 'arrow-red' };
+        }
+
+
+        this.board.addArrow(arrowType, startSquare, endSquare);
+      }
+
+
+      if (event.type === SQUARE_SELECT_TYPE.primary && event.mouseEvent.type === 'mousedown') {
+
+        if (!this.chessInstance.get(event.square)) {
+          this.board.removeArrows();
+          this.board.removeMarkers();
+        }
+
+      }
+
+      if (event.type === SQUARE_SELECT_TYPE.secondary && event.mouseEvent.type === 'mousedown') {
+
+        let classCircle = 'marker-circle-green';
+
+        if (ctrKeyPressed) {
+          classCircle = 'marker-circle-red';
+        } else if (shiftKeyPressed) {
+          classCircle = 'marker-circle-blue';
+        } else if (altKeyPressed) {
+          classCircle = 'marker-circle-yellow';
+        }
+
+        let myOwnMarker = { class: classCircle, slice: 'markerCircle' };
+
+        if (ctrKeyPressed && shiftKeyPressed && altKeyPressed) {
+          myOwnMarker = MARKER_TYPE.frame;
+        }
+
+
+        const markersOnSquare = this.board.getMarkers(undefined, event.square);
+        if (markersOnSquare.length > 0) {
+          this.board.removeMarkers(undefined, event.square);
+        } else {
+
+          this.board.addMarker(myOwnMarker, event.square);
+        }
+
+
+      }
+    });
+
+    this.turnRoundBoard(this.puzzleColor);
+
 
   }
 
