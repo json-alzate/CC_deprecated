33348255e548cb5357b386ebf7fcf4725efa34c0
json-alzate <developer.alzate@gmail.com>
2023-12-19 21:25:08 -0500

    Add puzzle time conversion logic

--- src/app/services/block.service.ts
+++ src/app/services/block.service.ts
@@ -45,6 +45,9 @@ export class BlockService {
 
     return new Promise((resolve, reject) => {
 
+      // Nota: si el tiempo del puzzle es mayor que el tiempo del bloque, el tiempo restante
+      // para el puzzle se convierte en el tiempo restante del bloque
+
       switch (option) {
         case 0: // Calentamiento
           //  2 minutos de mates en 1 (elo - 500) / tiempo por puzzle = 10 segundos
@@ -52,14 +55,14 @@ export class BlockService {
           // 1 ejercicio de mate
           break;
         case 5:
-          /*
+          /* No Muestra soluciones
               - tema random = t 2.5 minutos / 15 segundos por puzzle
               - tema debilidades (elo - 200) = t 2.5 minutos / 30 segundos por puzzle
           */
           break;
         case 10:
 
-          /**
+          /** No Muestra soluciones
            * - tema random || debilidades = t 2 minutos / 15 segundos por puzzle (elo - 100)
            * - apertura random || apertura débil = t 2 minutos / 30 segundos por puzzle
            * - misma apertura + mismo tema random = t 3 minutos / 60 segundos por puzzle
@@ -69,9 +72,29 @@ export class BlockService {
 
           break;
         case 20:
+          /** Muestra soluciones
+           * - debilidades = t 3 minutos / 40 segundos por puzzle (elo - 500)
+           * - tema random = t 5 minutos / 3 minutos por puzzle
+           * - mate en 1 = t 2 minutos / 30 segundos por puzzle
+           * - mismo tema random = t 2 minutos / 50 segundos por puzzle (elo - 300)
+           * - mismo tema random = t 2 minuto / 50 segundos por puzzle (elo + 200)
+           * - mismo tema random = t 1 minuto / 15 segundos por puzzle (elo - 800)
+           *    ||  mismo tema random a ciegas 10 segundos = t 1 minuto / 60 segundos por puzzle
+           * - finales = t 3 minutos / 60 segundos por puzzle
+           * - mate 3 = t 2 minutos / 60 segundos por puzzle
+           * */
 
           break;
         case 30:
+          /** Muestra soluciones
+           * - debilidades = t 5 minutos / 60 segundos por puzzle (elo - 200)
+           * - apertura random = t 2 minutos / 30 segundos por puzzle
+           * - tema random = t 5 minutos / 3 minutos por puzzle
+           * - mismo tema random  a ciegas 15 segundos = t 5 minutos / 50 segundos por puzzle (elo - 300)
+           * - finales = t 5 minutos / 60 segundos por puzzle
+           * - finales de peones = t 5 minutos / 3 minutos por puzzle
+           * - mate 4 o mas = t 2 minutos / 60 segundos por puzzle
+           * */
 
           break;
         default:
