45f0f04331b3fed85c1975eafe893bd867c4ac26
json-alzate <developer.alzate@gmail.com>
2024-03-21 12:51:29 -0500

    Update Firebase configuration in environment files

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,7 +1,20 @@
-import { keys } from './private/keys';
+let localKeys;
+if (!process.env.FIREBASE_apiKey) {
+  localKeys = require('./private/keys').keys;
+}
+
 
 export const environment = {
-  firebase: keys.firebase,
+  firebase: {
+    projectId: process.env.FIREBASE_projectId || localKeys.firebase.projectId,
+    appId: process.env.FIREBASE_appId || localKeys.firebase.appId,
+    storageBucket: process.env.FIREBASE_storageBucket || localKeys.firebase.storageBucket,
+    locationId: process.env.FIREBASE_locationId || localKeys.firebase.locationId,
+    apiKey: process.env.FIREBASE_apiKey || localKeys.firebase.apiKey,
+    authDomain: process.env.FIREBASE_authDomain || localKeys.firebase.authDomain,
+    messagingSenderId: process.env.FIREBASE_messagingSenderId || localKeys.firebase.messagingSenderId,
+    measurementId: process.env.FIREBASE_measurementId || localKeys.firebase.measurementId,
+  },
   production: false,
   environmentName: 'staging',
   apiPuzzlesUrl: 'https://puzzles.chesscolate.com/puzzles/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,25 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import { keys } from './private/keys';
+let localKeys;
+if (!process.env.FIREBASE_apiKey) {
+  localKeys = require('./private/keys').keys;
+}
+
 
 export const environment = {
   production: false,
   environmentName: 'dev',
-  firebase: keys.firebase,
+  firebase: {
+    projectId: process.env.FIREBASE_projectId || localKeys.firebase.projectId,
+    appId: process.env.FIREBASE_appId || localKeys.firebase.appId,
+    storageBucket: process.env.FIREBASE_storageBucket || localKeys.firebase.storageBucket,
+    locationId: process.env.FIREBASE_locationId || localKeys.firebase.locationId,
+    apiKey: process.env.FIREBASE_apiKey || localKeys.firebase.apiKey,
+    authDomain: process.env.FIREBASE_authDomain || localKeys.firebase.authDomain,
+    messagingSenderId: process.env.FIREBASE_messagingSenderId || localKeys.firebase.messagingSenderId,
+    measurementId: process.env.FIREBASE_measurementId || localKeys.firebase.measurementId,
+  },
   apiPuzzlesUrl: 'http://[::1]:3000/puzzles/',
   version: '1.0.1'
 };
