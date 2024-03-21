6400c769d0493507a004cf4a02c1bb7c20bbca45
json-alzate <developer.alzate@gmail.com>
2024-03-21 17:32:02 -0500

    deploy 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,15 @@
-let localKeys;
-if (!process?.env?.FIREBASE_apiKey) {
-  localKeys = require('./private/keys').keys;
-}
+
 
 export const environment = {
   firebase: {
-    projectId: process?.env?.FIREBASE_projectId || localKeys.firebase.projectId,
-    appId: process?.env?.FIREBASE_appId || localKeys.firebase.appId,
-    storageBucket: process?.env?.FIREBASE_storageBucket || localKeys.firebase.storageBucket,
-    locationId: process?.env?.FIREBASE_locationId || localKeys.firebase.locationId,
-    apiKey: process?.env?.FIREBASE_apiKey || localKeys.firebase.apiKey,
-    authDomain: process?.env?.FIREBASE_authDomain || localKeys.firebase.authDomain,
-    messagingSenderId: process?.env?.FIREBASE_messagingSenderId || localKeys.firebase.messagingSenderId,
-    measurementId: process?.env?.FIREBASE_measurementId || localKeys.firebase.measurementId,
+    projectId: process?.env?.FIREBASE_projectId,
+    appId: process?.env?.FIREBASE_appId,
+    storageBucket: process?.env?.FIREBASE_storageBucket,
+    locationId: process?.env?.FIREBASE_locationId,
+    apiKey: process?.env?.FIREBASE_apiKey,
+    authDomain: process?.env?.FIREBASE_authDomain,
+    messagingSenderId: process?.env?.FIREBASE_messagingSenderId,
+    measurementId: process?.env?.FIREBASE_measurementId,
   },
   production: true,
   environmentName: 'prod',
