ba4eb0efd575205a1edc9d031ec4098a1d4b663d
json-alzate <developer.alzate@gmail.com>
2024-04-18 19:24:17 -0500

    plan chart toolbar

--- src/app/pages/puzzles/components/plan-chart/plan-chart.component.ts
+++ src/app/pages/puzzles/components/plan-chart/plan-chart.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 
 import { ModalController } from '@ionic/angular';
+
+import { Chart, registerables } from 'chart.js';
+
 
 import { PlanTypes } from '@models/plan.model';
 import { Profile } from '@models/profile.model';
@@ -13,21 +16,29 @@ import { ProfileService } from '@services/profile.service';
   templateUrl: './plan-chart.component.html',
   styleUrls: ['./plan-chart.component.scss'],
 })
-export class PlanChartComponent implements OnInit {
+export class PlanChartComponent implements OnInit, AfterViewInit {
 
   @Input() planType: PlanTypes;
   profile: Profile;
   totalElo = 1500;
-  themes: { [key: string]: number } = {};
-  openings: string[] = [];
+  themesLabels: string[] = [];
+  themesElos: number[] = [];
+  openingsLabels: string[] = [];
+  openingsElos: number[] = [];
+
   constructor(
     private modalController: ModalController,
     private profileService: ProfileService
   ) {
-
+    Chart.register(...registerables);
   }
 
   ngOnInit() {
+
+
+  }
+
+  ngAfterViewInit() {
     this.profileService.subscribeToProfile().pipe().subscribe(profile => {
       this.profile = profile;
 
@@ -35,9 +46,22 @@ export class PlanChartComponent implements OnInit {
     const elos = this.profileService.getElosThemesByPlanType(this.planType);
     this.totalElo = this.profileService.getEloTotalByPlanType(this.planType);
     const openings = this.profileService.getElosOpeningsByPlanType(this.planType);
-    console.log(elos);
-    console.log(openings);
 
+    // ordenar elos de menor a mayor
+    const elosShort = Object.entries(elos).sort((a, b) => a[1] - b[1]);
+    const openingsShort = Object.entries(openings).sort((a, b) => a[1] - b[1]);
+
+    // get from shortened object
+
+    elosShort.forEach(([key, value]) => {
+      this.themesLabels = [...this.themesLabels, key];
+      this.themesElos = [...this.themesElos, value];
+    });
+
+    openingsShort.forEach(([key, value]) => {
+      this.openingsLabels = [...this.openingsLabels, key];
+      this.openingsElos = [...this.openingsElos, value];
+    });
   }
 
   close() {
