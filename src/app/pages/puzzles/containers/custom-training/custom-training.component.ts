47892fd9d658b6270bce61cf72b7f16eb5754cf9
json-alzate <developer.alzate@gmail.com>
2024-08-07 08:26:24 -0500

    custom plan component

---
+++ src/app/pages/puzzles/containers/custom-training/custom-training.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-training',
+  templateUrl: './custom-training.component.html',
+  styleUrls: ['./custom-training.component.scss'],
+})
+export class CustomTrainingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
