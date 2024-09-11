8f8733447d663baf4905599d5d96dd8ab2a46737
json-alzate <developer.alzate@gmail.com>
2024-09-11 13:21:47 -0500

    translates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const environment = {
   production: true,
   environmentName: 'prod',
   apiPuzzlesUrl: 'https://puzzles.chesscolate.com/puzzles/',
-  version: '1.2.3'
+  version: '1.3.0'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -27,7 +27,7 @@ export const environment = {
   environmentName: 'dev',
   firebase: keys.firebase,
   apiPuzzlesUrl: 'http://[::1]:3000/puzzles/',
-  version: '1.2.3'
+  version: '1.3.0'
 };
 
 
