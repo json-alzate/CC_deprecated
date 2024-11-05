d1aeb113393158de42c92ce68e820d79d0da8c10
json-alzate <developer.alzate@gmail.com>
2024-11-05 12:03:25 -0500

    history page

--- src/app/pages/puzzles/containers/plans-history/plans-history.component.ts
+++ src/app/pages/puzzles/containers/plans-history/plans-history.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { Observable } from 'rxjs';
+
+import { Plan, Block, PlanTypes } from '@models/plan.model';
+
+import { PlanService } from '@services/plan.service';
 @Component({
   selector: 'app-plans-history',
   templateUrl: './plans-history.component.html',
@@ -7,8 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlansHistoryComponent implements OnInit {
 
-  constructor() { }
+  plansHistory$: Observable<Plan[]>;
+  allPlans: Plan[];
+  plans: Plan[];
+  index = 0;
+  constructor(
+    private planService: PlanService,
+    private router: Router
+  ) {
+    this.planService.getPlansHistoryState().subscribe((plans) => {
+      this.allPlans = plans;
+      this.plans = this.allPlans.slice(this.index, this.index + 20);
+      this.index += 20;
+    });
 
-  ngOnInit() {}
+  }
+
+  ngOnInit() { }
+
+  onIonInfinite(infiniteScroll) {
+
+    setTimeout(() => {
+      this.plans = [...this.plans, ...this.allPlans.slice(this.index, this.index + 5)];
+      this.index += 5;
+      infiniteScroll.target.complete();
+    }, 500);
+  }
+
+  goToPlanDetails(plan) {
+    this.planService.setPlanAction(plan);
+    this.router.navigate(['/puzzles/plan-played']);
+  }
 
 }
