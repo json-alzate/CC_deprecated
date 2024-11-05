1b0d8ffa29431aab12bd64f2effaeccc74dca52a
json-alzate <developer.alzate@gmail.com>
2024-11-05 08:12:07 -0500

    Route all history in slide

---
+++ src/app/pages/puzzles/containers/plans-history/plans-history.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plans-history',
+  templateUrl: './plans-history.component.html',
+  styleUrls: ['./plans-history.component.scss'],
+})
+export class PlansHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
